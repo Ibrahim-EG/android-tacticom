4607e7b152e3897c15fa9a0c8030af1bdf9db58f
Ibrahim Khaled <Ibrahimkhaled2008@outlook.sa>
2026-09-04 20:18:25 +0300

    Refactor notification manager initialization
    Refactor notification manager initialization to avoid verifier crashes and use compatibility method for creating channels.

--- app/src/main/java/com/tacticom/app/App.kt
+++ app/src/main/java/com/tacticom/app/App.kt
@@ -1,8 +1,8 @@
 package com.tacticom.app
 
 import android.app.Application
-import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.content.Context
 import android.os.Build
 
 class App : Application() {
@@ -15,14 +15,12 @@ class App : Application() {
         super.onCreate()
         Store.init(this)
         Bus.dark.value = Store.themeDark
-        val nm = getSystemService(NotificationManager::class.java)
+        
+        // Use the old API 1 way to get the service to avoid verifier crashes
+        val nm = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            nm.createNotificationChannel(
-                NotificationChannel(CH_SERVICE, "Intercom Service", NotificationManager.IMPORTANCE_LOW)
-            )
-            nm.createNotificationChannel(
-                NotificationChannel(CH_RING, "Incoming Ring", NotificationManager.IMPORTANCE_HIGH)
-            )
+            Compat.createChannels(nm)
         }
     }
 }
